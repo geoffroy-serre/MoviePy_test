777d7151b4b0faec2303fef0efaa108c250a88b9
GMSAnubis <geff1982@gmail.com>
2021-04-04 04:52:54 +0200

    Created spaceEntity, trying to write objects infos on video

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from moviepy.editor import *
+from moviepy.config import change_settings
+from spaceentity import SpaceEntity
 
-video_galaxy = VideoFileClip("assets/galaxy.mp4")
-video_venus = VideoFileClip("assets/venus.mp4")
-video_terre = VideoFileClip("assets/terre.mp4")
-video_mars = VideoFileClip("assets/mars.mp4")
+change_settings({"IMAGEMAGICK_BINARY": r"imagemagick\\convert.exe"})
 
+space_entities = [
+    SpaceEntity("Terre", 6371, 15),
+    SpaceEntity("Venus", 6051, 462),
+    SpaceEntity("Mars", 3300, 63),
+    SpaceEntity("Galaxy", 50000000, 0)
+]
+
+videos_name = ['galaxy', 'mars', 'terre', 'venus']
+clips = []
+for entity in space_entities:
+    video_file_clips = VideoFileClip(f"assets/{entity.name}.mp4").subclip(0, 5)
+    name = TextClip(entity.name, fontsize=80, color='white').set_position((0.1, 0.5), relative=True
+                                                                          ).set_duration(
+        5),
+
+    composite = CompositeVideoClip([video_file_clips, name])
+
+    clips.append(composite)
+
+# assembler les vidéos
+final_clip = concatenate_videoclips(clips)
+
+# rendu
+final_clip.write_videofile("final_render.mp4", codec='libx264')
---
+++ spaceentity.py
@@ -0,0 +1,6 @@
+
+class SpaceEntity:
+    def __init__(self, name, size, temperature):
+        self.name = name
+        self.size = size
+        self.temperature = temperature
