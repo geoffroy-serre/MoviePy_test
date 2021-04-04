e1c8b9a6b66eba6f42525e271c0c828c69c4c684
GMSAnubis <geff1982@gmail.com>
2021-04-04 03:00:10 +0200

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+from moviepy.editor import *
+
+video_galaxy = VideoFileClip("assets/galaxy.mp4")
+video_venus = VideoFileClip("assets/venus.mp4")
+video_terre = VideoFileClip("assets/terre.mp4")
+video_mars = VideoFileClip("assets/mars.mp4")
+
